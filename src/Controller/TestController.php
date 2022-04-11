aea811c1b7a6115bdb1137c894f32b5cdd82989e
HAjulien <hajulien1@gmail.com>
2022-04-11 13:36:25 +0200

    first commit

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -14,6 +14,9 @@ class TestController extends AbstractController
         return $this->render('test/index.html.twig', [
             'controller_name' => 'Mon TestController',
         ]);
+        /** 
+        *    return $this->json(['username' => 'jane.doe']);
+        */
     }
 
     /** 
@@ -22,8 +25,10 @@ class TestController extends AbstractController
     #[Route("/test/{id}", name:"test_id", methods:["GET"], requirements:["id"=>"\d+"])]
     public function test_id($id): Response
     {
+        // dd($id); (var_) dump and die
         return $this->render('test/test.html.twig', [
             'controller_name' => 'Mon TestController avec id',
+            'id' => $id,
         ]);
     }
 }
